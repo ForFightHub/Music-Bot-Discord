39d065cef981f880bb79679f69efd1db72ee0614
For Fight <113169031+ForFightHub@users.noreply.github.com>
2023-04-30 23:26:02 +0330

    Add files via upload

---
+++ commands/resume.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../util/error");
+
+module.exports = {
+  info: {
+    name: "resume",
+    description: "To resume the paused music",
+    usage: "",
+    aliases: [],
+  },
+
+  run: async function (client, message, args) {
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (serverQueue && !serverQueue.playing) {
+      serverQueue.playing = true;
+      serverQueue.connection.dispatcher.resume();
+      let xd = new MessageEmbed()
+      .setDescription("▶ Resumed the music for you!")
+      .setColor("YELLOW")
+      .setAuthor("Music has been Resumed!", "https://github.com/ForFightHub/Music-Bot-Discord/blob/main/assets/Music.png?raw=true")
+      return message.channel.send(xd);
+    }
+    return sendError("There is nothing playing in this server. Play Not Resume!", message.channel);
+  },
+};
